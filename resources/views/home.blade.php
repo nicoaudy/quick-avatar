0b2f6dc5f89bf95b75c05d5f310947d1a9ae5d8e
nicoaudy <nicoaudy1994@gmail.com>
2017-09-21 19:12:33 +0700

    setup blade for profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,24 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Profile</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <form action="" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('PATCH') }}
 
-                    You are logged in!
+                        <div class="form-group">
+                            avatar
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') ?? Auth::user()->name }}">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default">Save</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
